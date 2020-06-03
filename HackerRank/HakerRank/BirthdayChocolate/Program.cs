81e4db1f0e559bbe36bb0973f08549c0d25b3480
Iran Lopes dos Santos <isantos@springglobal.com>
2020-06-03 17:33:46 -0300

    Adicionado BirthdayChocolate

---
+++ HackerRank/HakerRank/BirthdayChocolate/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BirthdayChocolate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> a = new List<int> {1,2,1,3,2};
+            System.Console.WriteLine(birthday(a, 3, 2));
+        }
+
+
+        static int birthday(List<int> s, int d, int m)
+        {
+
+            int sum = 0, count = 0, limit = 0;
+
+            for (int i = 0; i < s.Count; i++)
+            {
+                sum = s[i];
+                limit = 1;
+
+                if(sum==d && m==limit){
+                    count++;
+                    continue;  
+                } 
+                
+                for (int j = i+1; j < s.Count; j++)
+                {
+                    limit++;
+                    if(limit > m) break;
+                    if((sum+=s[j])> d) break;
+                    if(sum==d && m==limit){
+                      count++;
+                      break;  
+                    } 
+                    
+                }
+            }
+
+            return count;
+        }
+    }
+}
--- HackerRank/HakerRank/BreakingTheRecords/Program.cs
+++ HackerRank/HakerRank/BreakingTheRecords/Program.cs
@@ -4,9 +4,35 @@ namespace BreakingTheRecords
 {
     class Program
     {
-        static void Main(string[] args)
+    static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] a = new int[] {3,4,21,36,10,28,35,5,24,42};
+            
+            int[] result = breakingRecords(a);
+
+            System.Console.WriteLine($"{result[0]} {result[1]}");
+        }
+
+        static int[] breakingRecords(int[] scores){
+            int min = scores[0];
+            int max = scores[0];
+            int countMin = 0;
+            int countMax = 0;
+
+            for (int i = 0; i < scores.Length; i++)
+            {
+                if(min>scores[i]){
+                    min = scores[i];
+                    countMin++;
+                }
+
+                if(max<scores[i]){
+                    max = scores[i];
+                    countMax++;
+                }
+            }
+
+            return new int[] {countMax, countMin};
         }
     }
 }
