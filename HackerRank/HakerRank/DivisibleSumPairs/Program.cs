3a521d31e16513412c8ddf1baf2aca618f127e45
Iran Lopes dos Santos <isantos@springglobal.com>
2020-06-04 11:30:56 -0300

    DivisibleSumPairs

---
+++ HackerRank/HakerRank/DivisibleSumPairs/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DivisibleSumPairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[] {1,3,2,6,1,2};
+            System.Console.WriteLine(divisibleSumPairs(a.Length, 3, a));
+        }
+
+        static int divisibleSumPairs(int n, int k, int[] ar)
+        {
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i+1; j < n; j++)
+                {
+                    if((ar[i] + ar[j]) % k == 0 ) count++;
+                }
+            }
+            
+            return count;
+        }
+    }
+}
