5862b5871d057ff425682860296ae6a004cd938c
Iran Lopes dos Santos <isantos@springglobal.com>
2020-06-04 14:55:09 -0300

    JumpingOnTheClouds

---
+++ HackerRank/HakerRank/JumpingOnTheClouds/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace JumpingOnTheClouds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] c = new int[] { 0, 0, 0, 0, 1, 0 };
+            System.Console.WriteLine(jumpingOnClouds(c));
+        }
+
+        static int jumpingOnClouds(int[] c)
+        {
+            int count = 0;
+            for (int i = 0; i < c.Length; i++)
+            {
+                if ((i + 2) < c.Length && (c[i + 2] == 0))
+                {
+                    count++;
+                    i++;
+                }
+                else
+                {
+                    if (i + 1 < c.Length)
+                    {
+                        if (c[i + 1] == 0)
+                            count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+
+    }
+}
