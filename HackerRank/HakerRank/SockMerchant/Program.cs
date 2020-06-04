91f644291e4e76aaa568d0501c440e2bec665012
Iran Lopes dos Santos <isantos@springglobal.com>
2020-06-04 12:03:19 -0300

    SockMerchant

---
+++ HackerRank/HakerRank/SockMerchant/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SockMerchant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[] {10,20,20,10,10,30,50,10,20};
+
+            System.Console.WriteLine(sockMerchant(a.Length, a));
+        }
+
+        static int sockMerchant(int n, int[] ar)
+        {
+            int pairs = 0;
+            int count = 1;
+
+            Array.Sort(ar);
+
+            int i = 0;
+            while (i<n)
+            {
+                int j = i+1;
+
+                while(j<n && ar[i]==ar[j])
+                {
+                    count++;
+                    j++;
+                }
+
+                pairs+=(int)count/2;
+
+                i=j;
+                count = 1;
+            }
+
+            return pairs;
+        }
+    }
+}
