7b1682f5d2d2b8d4791473b6e0bef11b8ef333cb
hierope508 <irannlopes@gmail.com>
2020-06-28 19:17:27 -0300

    Mais desafios

---
+++ HackerRank/HakerRank/2DArrayDS/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _2DArrayDS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int[][] arr = new int[][] {
+                new int[] {-1,-1,0,-9,-2,-2},
+                new int[] {-2,-1,-6,-8,-2,-5},
+                new int[] {-1,-1,-1,-2,-3,-4},
+                new int[] {-1,-9,-2,-4,-4,-5},
+                new int[] {-7,-3,-3,-2,-9,-9},
+                new int[] {-1,-3,-1,-2,-4,-5},
+            };
+
+            Console.WriteLine(hourglassSum(arr));
+        }
+
+        static int hourglassSum(int[][] arr)
+        {
+            int max = Int32.MinValue;
+            for (int i = 0; i <4; i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < 4; j++)
+                {
+                    sum = 0;
+
+                    for (int k = j; k < j+3; k++)
+                    {
+                        sum += arr[i][k];
+                    }
+
+                    sum += arr[i+1][j+1];
+                    
+                    for (int k = j; k < j + 3; k++)
+                    {
+                        sum += arr[i+2][k];
+                    }
+
+                    if (max < sum) max = sum;
+                }
+
+
+            }
+
+            return max;
+        }
+    }
+}
---
+++ HackerRank/HakerRank/LeftRotation/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace LeftRotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 33, 47, 70, 37, 8, 53, 13, 93, 71, 72, 51, 100, 60, 87, 97 };
+            
+            print(arr);
+            
+            arr = rotLeft(arr, 13);
+            
+            print(arr);
+        }
+
+        static void print(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i]);
+                Console.Write(" ");
+            }
+
+            Console.WriteLine();
+        }
+
+        static int[] rotLeft(int[] a, int d)
+        {
+
+            int[] result = new int[a.Length];
+            for (int i = 0; i < a.Length; i++)
+            {
+                int newPosition = (a.Length - d) + i;
+
+                if (newPosition >= a.Length)
+                    newPosition = newPosition - a.Length;
+
+                result[newPosition] = a[i];
+            }
+
+            return result;
+ 
+        }
+    }
+}
