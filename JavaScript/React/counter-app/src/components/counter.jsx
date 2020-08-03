25d75be7bb6fc68db13ba25ad8f4923ec6afb5d0
Iran Lopes dos Santos <isantos@springglobal.com>
2020-08-03 19:21:10 -0300

    Counters

--- JavaScript/React/counter-app/src/components/counter.jsx
+++ JavaScript/React/counter-app/src/components/counter.jsx
@@ -2,11 +2,11 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
   };
 
   handleIncrement = ()=>{
-    this.setState({count: this.state.count+1});
+    this.setState({value: this.state.value+1});
   }
 
   render() {
@@ -22,12 +22,12 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount(){
-      let {count} = this.state;
+      let {value: count} = this.state;
       return count === 0 ? "Zero" : count;
   }
 }
